0e7d03e6cbf0aca922e4a1568051fa3615aa7308
Davide Filippi <d91.filippi@gmail.com>
2024-03-15 18:21:11 +0100

    add speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Header } from "@/app/ui";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={`${inter.className} h-full`}>
         <Header />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
